774c60893e72c2dc68bb12662e8fdec8369a9189
Антон Ворошилов <108758569+Palazaram@users.noreply.github.com>
2023-08-31 16:57:25 +0300

    Created the nUnit test for FindPosition method

---
+++ ToolsTest/AntonTests/nUnit.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using Tools;
+
+namespace ToolsTest.AntonTests
+{
+    internal class nUnit
+    {
+        [Test]
+        public void FindPosition_ReturnsNegativeOneForNullInput()
+        {
+            // Arrange
+            string text = null;
+            string findingText = "Hello, world!";
+
+            // Act
+            int result = TextTool.FindPosition(text, findingText);
+
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void FindPosition_ReturnsZeroForEmptyInputs()
+        {
+            // Arrange
+            string text = "";
+            string findingText = "";
+
+            // Act
+            int result = TextTool.FindPosition(text, findingText);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void FindPosition_ReturnsIndexForSingleMatch()
+        {
+            // Arrange
+            string text = "Hello, world!";
+            string findingText = "Hello";
+
+            // Act
+            int result = TextTool.FindPosition(text, findingText);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void FindPosition_ReturnsNegativeOneForNoMatch()
+        {
+            // Arrange
+            string text = "Hello, world!";
+            string findingText = "World";
+
+            // Act
+            int result = TextTool.FindPosition(text, findingText);
+
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+    }
+}
