588ef9b728231992e3819d8e4de4b8dcfd50a30e
Антон Ворошилов <108758569+Palazaram@users.noreply.github.com>
2023-08-31 18:01:58 +0300

    Created back class UnitTest1

---
+++ ToolsTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿// Це шаблон для побудови класу тестів
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using Xunit;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using nAssert = NUnit.Framework.Assert;
+using xAssert = Xunit.Assert;
+using Theory = Xunit.TheoryAttribute;
+
+namespace ToolsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+}
