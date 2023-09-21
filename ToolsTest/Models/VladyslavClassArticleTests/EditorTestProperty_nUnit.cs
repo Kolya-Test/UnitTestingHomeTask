c6091dc305d65d5cf26b750cd1fdcfb957abefc2
Vladyslav_Slyzkouhyi <Vladyslav.Slyzkouhyi@gmail.com>
2023-09-21 10:00:54 +0300

    Connection test

---
+++ ToolsTest/Models/VladyslavClassArticleTests/EditorTestProperty_nUnit.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolsTest.VladyslavClassArticleTests
+{
+    internal class EditorTestProperty_nUnit
+    {
+    }
+}
---
+++ ToolsTest/Models/VladyslavClassArticleTests/UpdateTextTest_MSTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolsTest.VladyslavClassArticleTests
+{
+    internal class UpdateTextTest_MSTest
+    {
+    }
+}
