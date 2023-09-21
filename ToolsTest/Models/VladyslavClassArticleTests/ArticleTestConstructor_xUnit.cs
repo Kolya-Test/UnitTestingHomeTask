2edea8f00c49a554f4421a89ce867debe45bb559
Vladyslav_Slyzkouhyi <Vladyslav.Slyzkouhyi@gmail.com>
2023-09-21 17:36:57 +0300

    Add 9 tests 3 for testing testing UpdaText() 3 for testing property Editor 3 for testing ctor Article ^O

--- ToolsTest/Models/VladyslavClassArticleTests/ArticleTestConstructor_xUnit.cs
+++ ToolsTest/Models/VladyslavClassArticleTests/ArticleTestConstructor_xUnit.cs
@@ -1,10 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Tools.Models;
+using Xunit;
 
-namespace ToolsTest.VladyslavClassArticleTests
+namespace ToolsTest.Models.VladyslavClassArticleTests
 {
-    internal class ArticleTestConstructor_xUnit
+    public class ArticleTestConstructor_xUnit
     {
+        [Fact]
+        public void CtorArticle_HasType_User()
+        {
+            //Arrange
+            Article _article = new Article();
+            User _user = new User("Tommy");
+            //Act
+            _article.Autor = _user;
+            //Assert
+            Assert.IsType<User>(_article.Autor);
+        }
+        [Fact]
+        public void CtorArticle_HasTitleAndText_TypeString()
+        {
+            //Arrange
+            Article _article = new Article();
+            //Act
+            _article.Title = "Title";
+            _article.Text = "Text";
+            //Assert
+            Assert.IsType<string>(_article.Title);
+            Assert.IsType<string>(_article.Text);
+        }
+        [Fact]
+        public void Ctor_AutorTitleText_NotNull()
+        {
+            //Arrange
+            Article _article = new Article();
+            //Act
+            _article.Autor = new User("Bill");
+            _article.Title = "Title";
+            _article.Text = "Text";
+            //Assert
+            Assert.NotNull(_article.Autor);
+            Assert.NotNull(_article.Title);
+            Assert.NotNull(_article.Text);
+        }
     }
 }
--- ToolsTest/Models/VladyslavClassArticleTests/EditorTestProperty_nUnit.cs
+++ ToolsTest/Models/VladyslavClassArticleTests/EditorTestProperty_nUnit.cs
@@ -1,10 +1,44 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
+using Tools.Models;
 
-namespace ToolsTest.VladyslavClassArticleTests
+namespace ToolsTest.Models.VladyslavClassArticleTests
 {
-    internal class EditorTestProperty_nUnit
+    [TestFixture]
+    public class EditorTestProperty_nUnit
     {
+        [Test]
+        public void Editor_Property_Type ()
+        {
+            //Arrange
+            Article _editor = new Article ();
+            //Act
+            _editor.Editor = new User("Peter");
+            //Assert
+            Assert.IsInstanceOf<User>(_editor.Editor);
+        }
+        [Test]
+        public void Editor_Name_NotNull()
+        {
+            //Arrange
+            Article _editor = new Article();
+            //Act
+            _editor.Editor = new User("SomeName");
+            //Assert
+            Assert.IsNotNull( _editor.Editor.Name);
+        }
+        [Test]
+        public void Editor_Name_TypeString()
+        {
+            //Arrange
+            Article _editor = new Article();
+            //Act
+            _editor.Editor = new User("NameHasTypeString");
+            //Assert
+            Assert.IsInstanceOf<string>(_editor.Editor.Name);
+        }
     }
 }
