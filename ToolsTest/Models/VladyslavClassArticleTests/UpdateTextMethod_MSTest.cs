da7f243d51c5bd04a11213cdf67d46b972b6e2ae
Vladyslav_Slyzkouhyi <Vladyslav.Slyzkouhyi@gmail.com>
2023-09-21 17:51:22 +0300

    Add UpdateText Test Method

---
+++ ToolsTest/Models/VladyslavClassArticleTests/UpdateTextMethod_MSTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tools.Models;
+
+namespace ToolsTest.Models.VladyslavClassArticleTests
+{
+    [TestClass]
+    public class UpdateTextMethod_MSTest
+    {
+        [TestMethod]
+        public void UpdateText_Text_NotNull()
+        {
+            //Arrange
+            Article _article = new Article();
+            string _text = "Hello";
+            //Act
+            _article.UpdateText(_text);
+            //Assert
+            Assert.IsNotNull(_article);
+        }
+        [TestMethod]
+        public void UpdateText_TextAndUser_NotNull()
+        {
+            //Arange 
+            Article _article = new Article();
+            _article.Text = "Test DONE";
+            _article.Editor = new User("John");
+            //Act
+            _article.UpdateText(_article.Text, _article.Editor);
+            //Assert
+            Assert.IsNotNull(_article.Text);
+            Assert.IsNotNull( _article.Editor);
+        }
+        [TestMethod]
+        public void UpdateText_Text_IsString()
+        {
+            //Arrange
+            Article _article = new Article();
+            _article.Text = "some text with type string!";
+            //Act
+            _article.UpdateText(_article.Text);
+            //Assert
+            Assert.IsInstanceOfType(_article.Text, typeof(string));
+        }
+    }
+}
